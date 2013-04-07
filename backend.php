a80d9472e28e854140f40cfbc1180527becea132
Spencer Florence <florence@ccs.neu.edu>
2013-04-07 00:12:15 +0000

    added most of the querying

--- backend.php
+++ backend.php
@@ -16,6 +16,28 @@ function disconnect($link) {
   mysql_close($link);
 }
 
+// Int String String -> Boolean
+function does_user_has_access($uid,$uri,$action){
+  $user_groups = get_user_groups($uid);
+  $permission_set_groups = get_permission_set_groups(get_permission_sets($uri,$action));
+  for($user_groups as $id) {
+    if(in_array($id,$permission_set_groups)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// [Array Ints] -> [Arrayof Ints]
+function get_permission_set_groups($ids){
+  return mysql_fetch_array(mysql_query("SELECT group_id FROM permission_sets WHERE id in (\"" . (implode("\", \"",$ids) . "\")"));
+}
+
+// String String -> [Arrayof Ints]
+function get_permission_sets($uri,$action){
+  return mysql_fetch_array(mysql_query("SELECT group_id FROM permission_sets WHERE resource_uri = \"$url\" AND action_name = \"$action\""));
+}
+
 // Int -> Arrayof Int
 function get_group_groups($id) {
   return get_groups_groups(array($id));
@@ -56,4 +78,18 @@ function get_user_id($user) {
 // String -> Int
 function get_group_id($group){
   return mysql_fetch_array(mysql_query("SELECT id FROM groups WHERE name = $group"))[0];
+}
+
+// -> Arrayof Strings
+function get_all_users() {
+  return mysql_fetch_array(mysql_query("SELECT name FROM users"));
+}
+function get_all_groups(){
+  return mysql_fetch_array(mysql_query("SELECT name FROM groups"));
+}
+function get_all_resources(){
+  return mysql_fetch_array(mysql_query("SELECT uri FROM resources"));
+}
+function get_all_actions(){
+  return mysql_fetch_array(mysql_query("SELECT name FROM actions"));
 }
