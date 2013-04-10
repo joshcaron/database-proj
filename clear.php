35513555d8570abf0b5f54418a62893c2aaa557a
Joshua Caron <jcaron@ccs.neu.edu>
2013-04-10 15:20:26 -0400

    Debugged init and clear

--- clear.php
+++ clear.php
@@ -3,14 +3,26 @@
 include_once "backend.php";
 
 connect();
-$users = get_all_users();
-foreach($users as $name {
-    $id = get_user_id($name);
-    delete_user($id);
+function clear_all() {
+    $users = get_all_users();
+
+    foreach($users as $name) {
+        $id = get_user_id($name);
+        delete_user($id);
+    }
+
+    $groups = get_all_groups();
+
+    foreach($groups as $group) {
+        $id = get_group_id($group);
+        delete_group($id);
+    }
+
+    // $resources = get_all_resources();
+    // foreach($resources as $resource) {
+    //     delete_resource($resource);
+    // }
 }
 
-$groups = get_all_groups();
-foreach($groups as $group) {
-    $id = get_group_id($group);
-    delete_group($id);
-}
+
+clear_all();
--- init.php
+++ init.php
@@ -3,11 +3,14 @@
 include_once "backend.php";
 
 connect();
+$group = array('');
+create_user("Josh", $group);
+create_user("Jeff");
+create_user("Paul");
+echo "Created Users!";
+
+
 echo "Connected!";
 create_group("Group A");
 create_group("Group B");
 echo "Created groups!";
-create_user("Josh");
-create_user("Jeff");
-create_user("Paul");
-echo "Created Users!";
