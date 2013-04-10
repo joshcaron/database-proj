2e2146dac25dd3344bcf78b97ab76c0b610ab96e
Joshua Caron <jcaron@ccs.neu.edu>
2013-04-10 02:26:27 -0400

    Added debugging for init

--- init.php
+++ init.php
@@ -3,9 +3,11 @@
 include_once "backend.php";
 
 connect();
-
+echo "Connected!";
 create_group("Group A");
 create_group("Group B");
+echo "Created groups!";
 create_user("Josh");
 create_user("Jeff");
 create_user("Paul");
+echo "Created Users!";
