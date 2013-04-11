9da4d7184b0a3e29a88d007fce59dc1521596eea
Joshua Caron <jcaron@ccs.neu.edu>
2013-04-11 00:01:07 -0400

    Removed comments in query that were causing issues.

--- start.php
+++ start.php
@@ -4,49 +4,19 @@ include_once "backend.php";
 
 connect();
 
-$sql = "-- MySQL dump 10.13  Distrib 5.5.29, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: db_final
--- ------------------------------------------------------
--- Server version   5.5.29-0ubuntu0.12.04.2
+$sql = "
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `actions`
---
 
 DROP TABLE IF EXISTS `actions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `actions` (
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `actions`
---
 
 LOCK TABLES `actions` WRITE;
-/*!40000 ALTER TABLE `actions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `actions` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `group_group_mapping`
---
-
 DROP TABLE IF EXISTS `group_group_mapping`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -58,47 +28,21 @@ CREATE TABLE `group_group_mapping` (
   CONSTRAINT `contained_fk` FOREIGN KEY (`contained_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE,
   CONSTRAINT `container_fk` FOREIGN KEY (`container_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `group_group_mapping`
---
 
 LOCK TABLES `group_group_mapping` WRITE;
-/*!40000 ALTER TABLE `group_group_mapping` DISABLE KEYS */;
-/*!40000 ALTER TABLE `group_group_mapping` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `groups`
---
-
 DROP TABLE IF EXISTS `groups`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `groups`
---
 
 LOCK TABLES `groups` WRITE;
-/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `permission_sets`
---
-
 DROP TABLE IF EXISTS `permission_sets`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `permission_sets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `is_allowed` tinyint(1) NOT NULL,
@@ -113,15 +57,8 @@ CREATE TABLE `permission_sets` (
   CONSTRAINT `resource_fk` FOREIGN KEY (`resource_uri`) REFERENCES `resources` (`uri`) ON DELETE CASCADE,
   CONSTRAINT `action_fk` FOREIGN KEY (`action_name`) REFERENCES `actions` (`name`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `permission_sets`
---
 
 LOCK TABLES `permission_sets` WRITE;
-/*!40000 ALTER TABLE `permission_sets` DISABLE KEYS */;
-/*!40000 ALTER TABLE `permission_sets` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -129,30 +66,15 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `resources`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `resources` (
   `uri` varchar(255) NOT NULL,
   PRIMARY KEY (`uri`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `resources`
---
 
 LOCK TABLES `resources` WRITE;
-/*!40000 ALTER TABLE `resources` DISABLE KEYS */;
-/*!40000 ALTER TABLE `resources` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `user_group_mapping`
---
-
 DROP TABLE IF EXISTS `user_group_mapping`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_group_mapping` (
   `user_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
@@ -161,50 +83,21 @@ CREATE TABLE `user_group_mapping` (
   CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `group_fk` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_group_mapping`
---
 
 LOCK TABLES `user_group_mapping` WRITE;
-/*!40000 ALTER TABLE `user_group_mapping` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_group_mapping` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `users`
---
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
 
 LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-04-06 23:04:46
 ";
 
 $success = mysql_query($query, $LINK);
