eb19335bf3743f03bc4f367ff04979b12f75392d
Joshua Caron <jcaron@ccs.neu.edu>
2013-04-12 02:54:06 -0400

    Get array from sql query

--- backend.php
+++ backend.php
@@ -195,6 +195,7 @@ function get_group_name($id) {
   $query = "SELECT name FROM groups WHERE id = $id";
   echo "QUERY: " . $query;
   $result = mysql_query($query);
+  $result = mysql_fetch_array($result)[0];
   echo "<br />RESULT: " . $result;
   return $result;
 }
