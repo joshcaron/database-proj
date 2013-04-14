99ebb7f8f89cee884b23827c88ce353874583fa2
Joshua Caron <jcaron@ccs.neu.edu>
2013-04-14 19:48:26 -0400

    Debugging creation again

--- create/create.php
+++ create/create.php
@@ -69,7 +69,9 @@ function created_group() {
     $user_ids = array();
     if ($group_users) {
         foreach($group_users as $user) {
+            echo "<BR />USER: " . $user;
             $id = get_user_id(urldecode($user));
+            echo "<BR />ID: " . $id;
             array_push($user_ids, $id);
         }
     }
@@ -78,7 +80,9 @@ function created_group() {
     $group_ids = array();
     if ($group_groups) {
         foreach($group_groups as $group) {
+            echo "<BR />GROUP: " . $group;
             $id = get_group_id(urldecode($group));
+            echo "<BR />ID: " . $id;
             array_push($group_ids, $id);
         }
     }
