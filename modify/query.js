55b36767e7d3be5807832fe959a3bedac8ee52dd
Joshua Caron <jcaron@ccs.neu.edu>
2013-04-14 17:36:23 -0400

    Confirm dialog before removing

--- modify/query.js
+++ modify/query.js
@@ -34,8 +34,29 @@ function manage_selections() {
 function delete_group() {
     $("tr").find(".group_del").each(function() {
         $(this).click(function() {
-            alert($(this).attr("id"));
-            $(this).parent().parent().slideUp();
+            var msg = "Are you sure you want to delete ";
+            msg += $(this).attr("id");
+            msg += "?";
+            var row = $(this).parent().parent();
+            $('<div></div>').appendTo('body')
+                    .html('<div><h6>Are you sure?</h6></div>')
+                    .dialog({
+                        modal: true, title: 'Delete message', zIndex: 10000, autoOpen: true,
+                        width: 'auto', resizable: false,
+                        buttons: {
+                            Yes: function () {
+                                $row.hide();
+
+                                $(this).dialog("close");
+                            },
+                            No: function () {
+                                $(this).dialog("close");
+                            }
+                        },
+                        close: function (event, ui) {
+                            $(this).remove();
+                        }
+                    });
         })
     });
 }
