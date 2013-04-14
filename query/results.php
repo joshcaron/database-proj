a35aaa91131224dbc69f22998803ff918d4d7b09
Joshua Caron <jcaron@ccs.neu.edu>
2013-04-14 19:43:15 -0400

    Made results a table

--- query/results.php
+++ query/results.php
@@ -13,25 +13,28 @@
         <div class="row-fluid">
             <div class="span2"></div>
             <div class="span8" id="inner_container">
-                <h1 id="title">Query Things</h1>
-                <?php
-                $user = urldecode($_POST["user"]);
-                $resource = urldecode($_POST["resource"]);
-                $action = urldecode($_POST["action"]);
-                $uid = get_user_id($user);
-                $allow = does_user_has_access($uid, $resource, $action);
-                if ($allow) {
-                    $allow = "True";
-                } else {
-                    $allow = "False";
-                }
-                echo "<p>User: " . $user . "</p>";
-                echo "<p>Resource: " . $resource . "</p>";
-                echo "<p>Action: " . $action . "</p>";
-                echo "<p>Allow: " . $allow . "</p>";
-
-                ?>
-                <a href="..">Back</a>
+                <div id="results">
+                    <h1 id="title">Query Things</h1>
+                    <?php
+                    $user = urldecode($_POST["user"]);
+                    $resource = urldecode($_POST["resource"]);
+                    $action = urldecode($_POST["action"]);
+                    $uid = get_user_id($user);
+                    $allow = does_user_has_access($uid, $resource, $action);
+                    if ($allow) {
+                        $allow = "True";
+                    } else {
+                        $allow = "False";
+                    }
+                    echo "<table><tbody>";
+                    echo "<tr><td>User</td><td>" . $user . "</td></tr>";
+                    echo "<tr><td>Resource</td><td>" . $resource . "</td></tr>";
+                    echo "<tr><td>Action</td><td>" . $action . "</td></tr>";
+                    echo "<tr><td>Allow</td><td>" . $allow . "</tr>";
+                    echo "</tbody></table>";
+                    ?>
+                    <a href="..">Back</a>
+                </div>
             </div>
         <div class="span2"></div>
         </div>
