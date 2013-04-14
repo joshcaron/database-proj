a2bf93b2a57f3a8896a5610b6c664e10ce0c5607
Joshua Caron <jcaron@ccs.neu.edu>
2013-04-14 15:50:54 -0400

    Debugging

--- backend.php
+++ backend.php
@@ -136,6 +136,7 @@ function delete_user_from_group($contained,$container) {
 // Int -> [Arrayof Int]
 function users_in_group($id) {
   $query = "SELECT user_id FROM user_group_mapping WHERE group_id = \"$id\"";
+  echo $query;
   $result = mysql_query($query);
   if (!$result) {
     echo "ERROR: " . mysql_error();
